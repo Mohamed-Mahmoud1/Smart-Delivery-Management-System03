43153f9710b897ef32ee5d31ad784afd205f979b
Mohamed-Mahmoud1 <mohamedzanaty.dev@gmail.com>
2026-09-16 22:53:20 +0300

    Part 01 — Theoretical Questions-Question01

--- Smart Delivery Management System03/Program.cs
+++ Smart Delivery Management System03/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01 — Theoretical Questions
+
+            #region Question01
+            //a)  What is the difference between Method Overloading and Method Overriding?
+            //Answer: Overloading is static polymorphism and means having the same method name but a different signature. Overriding is dynamic polymorphism, which means a method inherited from a parent class has a different implementation in the child class.
+
+            //b)  What is the difference between Static Binding and Dynamic Binding?
+            //Answer: Static binding is determined at compile time, such as method overloading, method hiding, and operator overloading. Dynamic binding is determined at runtime by the CLR, such as method overriding, abstract methods, and interfaces.
+
+            #endregion
+
+            #endregion
         }
     }
 }
